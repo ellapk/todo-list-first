e855cec0cfd0780128ba7ec84adc18e614b41209
xeangit <mily072@naver.com>
2021-10-12 10:00:49 +0900

    todo-list

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+   return (
+       <header>
+           <h1>To do List</h1>
+           <h1>What needs to be done?</h1>
+       </header>
+   );
+};
+ 
+export default Header;
